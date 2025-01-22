f671771b018b9ae9e8ef23a0908e1e9b60283c15
Salvanim <109810280+Salvanim@users.noreply.github.com>
2025-01-22 10:13:46 -0500

    round image

---
+++ PythonTesting/imageRounding.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+def roundImage(input_path, output_path, roundAmount):
+    try:
+
+        im = Image.open(input_path).convert('RGBA')
+        imageOutput = Image.new(im.mode, im.size)
+        imageList = list(im.getdata())
+        for pixelIndex in range(len(imageList)):
+            createdPixel = []
+            for val in range(len(imageList[pixelIndex])):
+                createdPixel.append((int(round(imageList[pixelIndex][val]/roundAmount))*roundAmount))
+            imageList[pixelIndex] = tuple(createdPixel)
+        imageOutput.putdata(imageList)
+        imageOutput.save(output_path)
+    
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The file '{input_path}' was not found.")
+
+roundImage("PythonTesting/test.png", "outputtest.png", 12)
